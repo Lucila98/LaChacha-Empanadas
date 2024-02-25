2e16044ab4a4c7980ab58e4aa2adfa59a86074cc
Lucila98 <Lula.nigro@gmail.com>
2024-02-25 15:09:25 -0300

    primer commit

---
+++ js/gustos.js
@@ -0,0 +1,36 @@
+const empanadasList = [
+    "Carne Suave",
+    "Carne Picante",
+    "Carne Cuchillo",
+    "Carne Criolla",
+    "Jamon y Queso",
+    "Jamon, Queso y Tomate",
+    "Pollo",
+    "Humita",
+    "Queso y Cebolla",
+    "Ananá, Jamon y Muzzarella",
+    "Roquefort",
+    "Roquefort con Jamon",
+    "Roquefort con Apio",
+    "Verdura",
+    "Muzzarella con Cantimpalo",
+    "Panceta y Ciruela",
+    "Oreganato",
+    "Albahaca",
+  ];
+
+  const pastelitosList = [
+    "Membrillo",
+    "Batata",
+    "Dulce de Leche",
+  ];
+
+  const bebidasList = [
+    { name: "Coca-cola 500cc", price: "$100" },
+    { name: "Sprite 500cc", price: "$100" },
+    { name: "Fanta 500cc", price: "$100" },
+    { name: "Stella Artois 473cc", price: "$300" },
+    { name: "Quilmes 473cc", price: "$300" },
+    { name: "Imperial 473cc", price: "$300" },
+    { name: "Bhrama 473cc", price: "$300" },
+  ];
